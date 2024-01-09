9632cde973c2ae81ed26f43ff0e7417d2f41855e
Ibrahim-Mujagic <mujagicibrahim1@gmail.com>
2024-01-09 16:40:11 +0100

    Added style of contacts

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import WhatsAppCardComp from "./components/WhatsAppCardComp.vue";
+export default {
+  name: "App",
+  components: {
+    WhatsAppCardComp,
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container-app">
+    <WhatsAppCardComp />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style lang="scss">
+@use "./styles/variables" as *;
+
+.container-app {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient($whatsapp-green 13%, $main-bg 13%);
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
---
+++ src/components/WhatsAppCardComp.vue
@@ -0,0 +1,174 @@
+<script>
+import { store } from "../data/store";
+import ChatCardComponent from "./ChatCardComponent.vue";
+export default {
+  name: "WhatsAppCard",
+  components: {
+    ChatCardComponent,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getBackgroundImage(image) {
+      return { backgroundImage: `url('${image}')` };
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="whatsApp-card">
+    <div class="header-card">
+      <div class="user-data">
+        <div class="user-pic">
+          <img :src="store.getImage('avatar_io.jpg')" alt="" />
+        </div>
+        <div class="user-name">
+          <h4>Sofia</h4>
+        </div>
+      </div>
+      <div class="friend-active-data">
+        <div class="friend-pic-name">
+          <img :src="store.getImage('avatar_1.jpg')" alt="" />
+          <div class="cont-text">
+            <h4>Michele</h4>
+            <p>Online •</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-main">
+      <div class="friends-list">
+        <div class="container-alerts">
+          <div class="icon">
+            <i class="fa-solid fa-bell"></i>
+          </div>
+          <div class="text-alert">
+            <strong>Ricevi notifiche di nuovi messaggi!</strong>
+            <p>Attiva notifiche desktop</p>
+          </div>
+        </div>
+        <div class="container-chats">
+          <ChatCardComponent
+            v-for="(item, index) in store.contacts"
+            :dataFriends="item"
+          />
+        </div>
+      </div>
+      <div
+        :style="getBackgroundImage(store.getImage('mine.jpg'))"
+        class="active-chat"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/variables" as *;
+.whatsApp-card {
+  height: 700px;
+  width: 60%;
+  border: 1px solid #000;
+
+  .header-card {
+    height: 70px;
+    background-color: $card-header-bg;
+    display: flex;
+
+    & > div {
+      height: 100%;
+    }
+
+    .user-data,
+    .friend-active-data {
+      display: flex;
+      align-items: center;
+      gap: 0px 10px;
+      padding: 8px;
+      img {
+        max-width: 40px;
+        border-radius: 50%;
+      }
+    }
+    .user-data {
+      width: $left-size-card-width;
+      border: 1px solid #a4a3a3;
+    }
+    .friend-active-data {
+      width: $chat-active-width;
+      flex-direction: column;
+      align-items: flex-start;
+      border-width: 1px 1px 1px 0px;
+      border-style: solid;
+      border-color: #a4a3a3;
+      justify-content: center;
+      .friend-pic-name {
+        display: flex;
+        align-items: center;
+        gap: 0px 10px;
+        margin-bottom: 3px;
+        p {
+          font-size: 0.7rem;
+          color: #a4a3a3;
+          cursor: default;
+        }
+      }
+    }
+  }
+  .card-main {
+    height: calc(100% - 70px);
+    display: flex;
+    .friends-list,
+    .active-chat {
+      height: 100%;
+    }
+
+    .friends-list {
+      width: $left-size-card-width;
+      background-color: #fff;
+      .container-alerts {
+        height: 60px;
+        background-color: $main-blue;
+        display: flex;
+        align-items: center;
+        gap: 0px 7px;
+        font-size: 0.8rem;
+        padding: 4px 15px;
+        .icon {
+          height: 40px;
+          width: 40px;
+          background-color: #fff;
+          border-radius: 50%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          i {
+            font-size: 1.5rem;
+            color: $main-blue;
+          }
+        }
+        .text-alert {
+          strong {
+            color: rgba(21, 21, 21, 0.85);
+          }
+          p {
+            text-decoration: underline;
+            color: #a4a3a3;
+          }
+        }
+      }
+      .container-chats {
+        width: 100%;
+        max-height: calc(100% - 60px);
+        overflow-y: auto;
+      }
+    }
+    .active-chat {
+      width: $chat-active-width;
+    }
+  }
+}
+</style>
